bbbef386d07799eeff59cd313459216ecae21b90
Chris Wander <chriswandermail@gmail.com>
2021-07-14 20:17:48 -0400

    ""

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
-export const App = ( )=> {
-    return <h1>React Typescript Starter Template</h1>
-}
+import './styles.css';
+import { ClickCounter } from './ClickCounter';
+
+export const App = () => {
+  const name = 'Chris';
+  return (
+    <div>
+      <h1>
+        React Typescript Starter Template - {process.env.NODE_ENV} -{name}
+        {process.env.name}
+      </h1>
+      <ClickCounter />
+    </div>
+  );
+};
